0a0cb7e8721f5c3f5281c61da2cb9ed088bc3170
Ducky <ducky@zio.sh>
2024-09-10 06:10:19 +0100

    add publish script

---
+++ publish.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+platforms=(
+    "linux-x64"
+    "win-x64"
+)
+
+base_dir="$(dirname "$(realpath -s "$0")")"
+git_tag="$(git describe --exact-match --tags)"
+git_commit="$(git rev-parse --short HEAD)"
+version=""
+
+if [[ -z $git_tag ]]; then
+    version="git-$git_commit"
+else    
+    version="$(echo $git_tag | sed 's:.*/::')"
+fi
+
+for platform in ${platforms[@]}; do
+    out_dir="$base_dir/bin/$version/$platform"
+    mkdir -p "$out_dir"
+
+    if [[ -d "$out_dir" ]]; then
+        for file in "$out_dir"/*; do
+            rm -rf "$file"
+        done
+    fi
+
+    echo "Publishing: $version ($platform)"
+    dotnet publish \
+        --configuration Release \
+        --output "$out_dir" \
+        --runtime $platform
+
+    if [[ $BOOSKI_PUBLISH_NO_POST_PUBLISH != 1 ]]; then
+        bin_name="Booski"
+        bin_ext="bin"
+
+        if [[ $platform == win* ]]; then
+            bin_name="$bin_name.exe"
+            bin_ext="exe"
+        fi
+
+        mv "$out_dir/$bin_name" "$out_dir/../booski-$version-$platform.$bin_ext"
+        rm -rf "$out_dir"
+    fi
+done
