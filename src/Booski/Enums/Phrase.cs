f8366f9703157afb4d2c417e737f3fd1b8a941d0
Ducky <ducky@zio.sh>
2024-09-27 20:01:09 +0100

    various

--- src/Booski/Enums/Phrase.cs
+++ src/Booski/Enums/Phrase.cs
@@ -22,6 +22,11 @@ public enum Phrase
     Console_PostHelpers_PostCrossposted,
     Console_PostHelpers_PostCrosspostedError,
     Console_PostHelpers_PostDeleted,
+    Console_PostHelpers_Ignoring,
+    Console_PostHelpers_IgnoringEmbedsButNotSupported,
+    Console_PostHelpers_IgnoringOldCreatedAtDate,
+    Console_PostHelpers_IgnoringReplyButNoParent,
+    Console_PostHelpers_IgnoringStartsWithMention,
     Console_StartCommand_ClientConnected,
     Console_StartCommand_ClientConnectedError,
     Console_StartCommand_DaemonAlreadyRunning,
@@ -30,6 +35,8 @@ public enum Phrase
     Console_StartCommand_FetchingPosts,
     Console_StartCommand_FetchingPostsError,
 
-    SecondUnit_Single,
-    SecondUnit_Multiple
+    Unit_Hour_Single,
+    Unit_Hour_Multiple,
+    Unit_Second_Single,
+    Unit_Second_Multiple
 }
